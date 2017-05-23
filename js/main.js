a79835b04e825b1eb0ac55ed079de0be708bc01e
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-22 23:17:14 -0400

    Ejercicio de esquemas realizado.

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var cuadroUno = document.getElementsByClassName("cuadrado-uno")[0];
+var cuadroDos = document.getElementsByClassName("cuadrado-dos")[0];
+
+cuadroUno.style.margin = "12px";
+cuadroDos.style.margin = "12px";
+
+var rojo = document.getElementsByClassName("rojo")[0];
+var morado = document.getElementsByClassName("morado")[0];
+var amarillo = document.getElementsByClassName("amarillo")[0];
+
+rojo.style.position = "relative";
+rojo.style.background = "#FF000C";
+rojo.style.width = "300px";
+rojo.style.height = "300px";
+
+morado.style.position = "absolute";
+morado.style.background = "#4105AF";
+morado.style.width = "150px";
+morado.style.height = "150px";
+morado.style.top = "75px";
+morado.style.left = "75px";
+
+amarillo.style.position = "absolute";
+amarillo.style.background = "#FFE71D";
+amarillo.style.width = "75px";
+amarillo.style.height = "75px";
+
+var azul = document.getElementsByClassName("azul")[0];
+var negro = document.getElementsByClassName("negro")[0];
+var verde = document.getElementsByClassName("verde")[0];
+
+azul.style.position = "relative";
+azul.style.background = "#1148FC";
+azul.style.width = "300px";
+azul.style.height = "300px";
+
+verde.style.position = "absolute";
+verde.style.background = "#2EBA31";
+verde.style.width = "150px";
+verde.style.height = "150px";
+verde.style.top = "75px";
+verde.style.left = "75px";
+
+negro.style.position = "absolute";
+negro.style.background = "#010101";
+negro.style.width = "75px";
+negro.style.height = "75px";
